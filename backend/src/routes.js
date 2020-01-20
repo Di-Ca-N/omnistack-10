b9fdc19d147d6c05a34fb9b59b10c0a1a4486ee8
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2020-01-20 20:23:33 -0300

    Ominstack DevRadar completed (with challenges and extras :3)

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+routes.get('/devs/', DevController.index);
+routes.post('/devs/', DevController.store);
+routes.patch('/devs/:github_username/', DevController.update);
+routes.delete('/devs/:github_username/', DevController.destroy);
+routes.get('/devs/:github_username/', DevController.show);
+
+routes.get('/search/', SearchController.index);
+
+module.exports = routes;
